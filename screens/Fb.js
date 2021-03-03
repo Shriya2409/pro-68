df03c08ef02ac856642685fba0bfb59063a2d7a3
Shriya2409 <72084377+Shriya2409@users.noreply.github.com>
2021-03-03 19:16:02 +0530

    Add files via upload

---
+++ screens/Fb.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Text, View, Button,TouchableOpacity, StyleSheet } from 'react-native';
+
+export default class Fb extends React.Component{
+render(){
+    return(
+        <View style={{
+            flex:1,
+            justifyContent:'center',
+            alignItems:'center'
+        }}>
+            <Text>Facebook</Text>
+        </View>
+    )
+}
+
+
+
+}
---
+++ screens/In.js
@@ -0,0 +1,19 @@
+import * as React from 'react';
+import { Text, View, Button,TouchableOpacity, StyleSheet } from 'react-native';
+
+export default class In extends React.Component{
+render(){
+    return(
+        <View style={{
+            flex:1,
+            justifyContent:'center',
+            alignItems:'center'
+        }}>
+            <Text>Instagram</Text>
+        </View>
+    )
+}
+
+
+
+}
